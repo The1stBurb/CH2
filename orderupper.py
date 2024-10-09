d7a82c118dad393fef4bb2d6b7c173eb88d86432
Etcetera_ <150498673+The1stBurb@users.noreply.github.com>
2024-10-09 18:16:08 +0000

    yay done

--- orderupper.py
+++ orderupper.py
@@ -16,11 +16,54 @@
 # Makes sure that the user has ordered at least 1 item
 # Allows user to place their order 
 # Allows user to change an item in their order
-itms={"drink":["water","wet water","soda"],"app":["salad","ice cream","apple"],"sid1":["sauce","apple sauce","frog soup"],"sid2":["icicles","food","beetles"],"dess":[]}
+itms={"drink":["water","wet water","soda","none"],"app":["salad","ice cream","apple","none"],"sid1":["sauce","apple sauce","frog soup","none"],"sid2":["icicles","food","beetles","none"],"dess":["salamanders","forks","spinach","walruses","none"]}
+prc={"water":1,"wet water":2,"soda":5,"none":0.01,"salad":1,"ice cream":5,"apple":2,"sauce":1,"apple sauce":3,"frog soup":7,"icicles":2,"food":1,"beetles":4.99,"salamanders":9.99,"forks":1,"spinach":5,"walruses":170,}
 class order:
-    def __init__(self):
-        self.drnk=""
-        self.app=""
-        self.sid1=""
-        self.sid2=""
-        self.dess=""
+    def __init__(self,drnk="",app="",sid1="",sid2="",dess=""):
+        self.drnk=drnk
+        self.app=app
+        self.sid1=sid1
+        self.sid2=sid2
+        self.dess=dess
+    def __str__(self):
+        return f"Drink: {self.drnk}\nAppateizer: {self.app}\nFirst Side: {self.sid1}\nSecond Side: {self.sid2}\nDessert: {self.dess}\nPrice: {prc[self.drnk]+prc[self.app]+prc[self.sid1]+prc[self.sid2]+prc[self.dess]}\n"
+    def ord1(self):
+        return self.drnk=="none"and self.app=="none"and self.sid1=="none"and self.sid2=="none"and self.dess=="none"
+    @classmethod
+    def dllr(self):
+        return order("water","salad","sauce","food","forks")
+def gt(inr):
+    while True:
+        inp=input("What would you like to get thats an "+{"drink":"drink","app":"appetizer","sid1":"first side","sid2":"second side","dess":"dessert"}[inr]+"? ")
+        if inp in itms[inr]:
+            print()
+            return inp
+        else:
+            print("Thats not an option!")
+def get():
+    for a,i in enumerate(itms):
+        print(["Drink","Appetizer","First Side","Second Side","Dessert"][a]+":")
+        for j in itms[i]:
+            print("  ",j+":",prc[j],"moneys")
+        print()
+    ord=order()
+    while True:
+        inp=input("WOuld you like to get the dollar menu?(y/n) ")
+        if inp=="y":
+            ord=order.dllr()
+            return ord
+        ord.drnk=gt("drink")
+        ord.app=gt("app")
+        ord.sid1=gt("sid1")
+        ord.sid2=gt("sid2")
+        ord.dess=gt("dess")
+        if ord.ord1():
+            print("You have to order something!")
+        else:
+            break
+    return ord
+ord=get()
+print(ord)
+inp=input("Are you happy with your order?(y/n) ")
+if inp!="y":
+    ord=get()
