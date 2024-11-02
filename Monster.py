88c9c6d4e0fd9e8218fc0134706c0cac925ac793
The1stBurb <eli.cooper@ucas-edu.net>
2024-11-01 20:21:01 -0600

    kljdskjdgas

--- Monster.py
+++ Monster.py
@@ -1,3 +1,4 @@
+from time import sleep
 # Make a superclass called monster that instantiates a name and where the monster is from and has a method for attack
 
 # Make sub classes for at least 4 different monsters that specify how that particular monster attacks
@@ -50,8 +51,45 @@ class squarle(monster):
     def atc(self,other):
         other.hp-=self.atk
         return f"{self.nm} {self.at} {other.nm} for {self.atk} damage"
-bobs=[nomNom("greg","alabama, new jersey"),burb("burb","eight foot house"),frog("tree","a tree"),angee("%$#^*!@","your soul"),ber("ber","georges packback"),squarle("frog","also tree"),]
+bobs=[nomNom("greg","alabama, new jersey"),burb("burb","eight foot house"),frog("tree","a tree"),angee("%$#^*!@","your soul"),]#ber("ber","georges packback"),squarle("frog","also tree"),]
 con=[]
-for i in range(0,len(bobs)-1,2):
-    con.append([bobs[i],bobs[i+1]])
+def conCon(bbs):
+    cn=[]
+    for i in range(0,len(bbs)-1,2):
+        cn.append([bbs[i],bbs[i+1]])
+    return cn
+con=conCon(bobs)
 print(con)
+def prBr(br):
+    for i in br:
+        print(i[0].nm,"vs",i[1].nm)
+    print("\n")
+while True:
+    prBr(con)
+    input("Press enter to continue!")
+    bobs=[]
+    for i in con:
+        op=i[0]
+        o=i[1]
+        print(op.nm,"vs",o.nm)
+        sleep(1)
+        fg=True
+        while fg:
+            print(op.atc(o))
+            if o.hp<=0:
+                print(o.nm,"has died!")
+                bobs.append(op)
+                fg=False
+                continue
+            print(o.atc(op))
+            if op.hp<=0:
+                print(op.nm,"has died!")
+                bobs.append(o)
+                fg=False
+                continue
+            sleep(0.5)
+        print("\n")
+    if len(bobs)==1:
+        break
+    con=conCon(bobs)
+print("The winner is: ",bobs[0].nm)
