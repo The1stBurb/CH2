69d82c0ae3b3f794906ba5fd790a787315d13fc7
The1stBurb <eli.cooper@ucas-edu.net>
2024-11-01 20:09:55 -0600

    asdgf

--- Monster.py
+++ Monster.py
@@ -3,49 +3,49 @@
 # Make sub classes for at least 4 different monsters that specify how that particular monster attacks
 class monster:
     def __init__(self,name,hom,hp,atk):
-        self.nm,self.hm,self.hp,self.atk=hame,hom.hp,atk
+        self.nm,self.hm,self.hp,self.atk=name,hom,hp,atk
     def atc(self):
         return "burb"
     def __str__(self):
         return f"{self.nm} from {self.hm} has {self.hp} health and {self.hp} attack!\n"
 class nomNom(monster):
     def __init__(self,nm,hm):
-        super.__init__(nm,hm,10,2)
+        super().__init__(nm,hm,10,2)
         self.at="noms on"
     def atc(self,other):
         other.hp-=self.atk
         return f"{self.nm} {self.at} {other.nm} for {self.atk} damage"
 class burb(monster):
     def __init__(self,nm,hm):
-        super.__init__(nm,hm,15,3)
+        super().__init__(nm,hm,15,3)
         self.at="pecks"
     def atc(self,other):
         other.hp-=self.atk
         return f"{self.nm} {self.at} {other.nm} for {self.atk} damage"
 class frog(monster):
     def __init__(self,nm,hm):
-        super.__init__(nm,hm,10,2)
+        super().__init__(nm,hm,10,2)
         self.at="hops over"
     def atc(self,other):
         other.hp-=self.atk
         return f"{self.nm} {self.at} {other.nm} for {self.atk} damage"
 class angee(monster):
     def __init__(self,nm,hm):
-        super.__init__(nm,hm,5,10)
+        super().__init__(nm,hm,5,10)
         self.at="screams at"
     def atc(self,other):
         other.hp-=self.atk
         return f"{self.nm} {self.at} {other.nm} for {self.atk} damage"
 class ber(monster):
     def __init__(self,nm,hm):
-        super.__init__(nm,hm,20,5)
+        super().__init__(nm,hm,20,5)
         self.at="potatos"
     def atc(self,other):
         other.hp-=self.atk
         return f"{self.nm} {self.at} {other.nm} for {self.atk} damage"
 class squarle(monster):
     def __init__(self,nm,hm):
-        super.__init__(nm,hm,1,2)
+        super().__init__(nm,hm,1,2)
         self.at="runs from"
     def atc(self,other):
         other.hp-=self.atk
