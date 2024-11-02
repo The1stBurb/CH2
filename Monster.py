fe8afad4109e968847ab409046cf4a8ff41f2bb3
The1stBurb <eli.cooper@ucas-edu.net>
2024-11-01 19:59:11 -0600

    kjdsgf

---
+++ Monster.py
@@ -0,0 +1,8 @@
+# Make a superclass called monster that instantiates a name and where the monster is from and has a method for attack
+
+# Make sub classes for at least 4 different monsters that specify how that particular monster attacks
+class monster:
+    def __init__(self,name,hom,hp,atk):
+        self.nm,self.hm,self.hp,self.atk=hame,hom.hp,atk
+    def atk(self):
+        return "burb"
